637bccaba7cadd836af49f1fecb50e4b0cdb1674
jakelauer <jake.lauer@gmail.com>
2015-10-08 12:13:12 -0700

    missing files

---
+++ BaseballTheater/Areas/Game/GameController.cs
@@ -0,0 +1,20 @@
+﻿using BaseballTheater.Areas.Game.Models;
+using System;
+using System.Globalization;
+using System.Web.Mvc;
+
+namespace BaseballTheater.Areas.Game
+{
+	public class GameController : Controller
+	{
+		[OutputCache(Duration = 60)]
+		public ActionResult Index(string date, int id)
+		{
+			var dateTime = DateTime.ParseExact(date, "yyyyMMdd", CultureInfo.InvariantCulture);
+
+			var model = new GameModel(dateTime, id);
+
+			return View(model);
+		}
+	}
+}
---
+++ BaseballTheater/Areas/Game/Models/GameModel.cs
@@ -0,0 +1,63 @@
+﻿using MlbDataServer.DataFetch;
+using MlbDataServer.DataStructures;
+using System;
+using System.Linq;
+using System.Net;
+
+namespace BaseballTheater.Areas.Game.Models
+{
+	public class GameModel
+	{
+		public GameSummary GameSummary { get; set; }
+		public HighlightsCollection HighlightsCollection { get; set; }
+
+		public GameModel(DateTime date, int gamePk)
+		{
+			var gameSummaryCreator = new GameSummaryCreator();
+			var gameCollection = gameSummaryCreator.GetSummaryCollection(date);
+			this.GameSummary = gameCollection.GameSummaries.FirstOrDefault(game => game.GamePk == gamePk);
+			if (this.GameSummary == default(GameSummary)) return;
+
+
+			var gameDetailCreator = new GameDetailCreator(this.GameSummary.GameDataDirectory);
+			HighlightsCollection = gameDetailCreator.GetHighlights();
+			if (HighlightsCollection == default(HighlightsCollection)) return;
+
+
+			foreach (var highlight in HighlightsCollection.Highlights)
+			{
+				for (var i = 0; i < highlight.Urls.Length; i++)
+				{
+					var url = highlight.Urls[i];
+
+					if (url.Contains("1200K"))
+					{
+						var testUrl = url.Replace("1200K", "1800K");
+						if (RemoteFileExists(testUrl))
+						{
+							highlight.Urls[i] = testUrl;
+						}
+					}
+				}
+			}
+		}
+
+		public bool RemoteFileExists(string url)
+		{
+			try
+			{
+				var request = WebRequest.Create(url) as HttpWebRequest;
+				if (request == null) return false;
+
+				request.Method = "HEAD";
+				var response = request.GetResponse() as HttpWebResponse;
+				return response != null && (response.StatusCode == HttpStatusCode.OK);
+			}
+			catch
+			{
+				//Any exception will returns false.
+				return false;
+			}
+		}
+	}
+}
