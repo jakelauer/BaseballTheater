b487836f46ae7f1d1fc6c1d974a000eb866166e7
jake_lauer <jake.lauer@gmail.com>
2016-05-08 17:24:16 -0700

    Removing RedditSharp from BaseballTheater

--- RedditSharp/FlairTemplate.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RedditSharp
-{
-    public class UserFlairTemplate // TODO: Consider using this class to set templates as well
-    {
-        public string Text { get; set; }
-        public string CssClass { get; set; }
-    }
-}
