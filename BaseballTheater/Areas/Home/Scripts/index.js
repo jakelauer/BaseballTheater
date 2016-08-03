8e5a9a33abbbeee16e3ddfc29d67e1b7b7a825d6
jakelauer <jake.lauer@gmail.com>
2016-08-02 22:11:57 -0700

    asdfamissin

---
+++ BaseballTheater/Areas/Home/Scripts/index.js
@@ -0,0 +1,10 @@
+﻿jQuery(function ($) {
+	var favoriteteam = Cookies.get("favoriteteam").split(",");
+
+	for (var i = 0; i < favoriteteam.length; i++)
+	{
+		var team = favoriteteam[i];
+		var $games = $(".game-summary[data-homecode=" + team + "], .game-summary[data-awaycode=" + team + "]");
+		$(".leagues").prepend($games);
+	}
+});
