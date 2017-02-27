832a2ecf69c7361344e600f4a0a1fed657b17009
Jake Lauer <jake.lauer@gmail.com>
2017-02-27 11:33:21 -0800

    Cleaning up inning display, adding patreon option

--- BaseballTheater/Areas/App/Scripts/Components/game_summary.ts
+++ BaseballTheater/Areas/App/Scripts/Components/game_summary.ts
@@ -1,5 +1,10 @@
 ﻿namespace Theater.Site
 {
+	var linescoreItem = (realNumber: string) =>
+	{
+		return App.Instance.settingsVueData.hideScores ? "▨" : realNumber;
+	};
+
 	Vue.component("game-summary",
 	{
 		template: $("template#game-summary").html(),
@@ -10,10 +15,7 @@
 				const dayString = game.dateObj.format("YYYYMMDD");
 				return `/game/${dayString}/${game.game_pk}`;
 			},
-			lineScoreItem: (realNumber: string) =>
-			{
-				return App.Instance.settingsVueData.hideScores ? "▨" : realNumber;
-			},
+			linescoreItem,
 			getStatusTime: (game: GameSummary) =>
 			{
 				//var eventTimeString = string.Format("{0}{1} {2}", game.EventTime, game.EventTimeAmPm, game.TimeZone);
@@ -22,6 +24,41 @@
 				var timezone = game.time_zone;
 
 				return `${eventTime}${ampm} ${timezone}`;
+			},
+			getInningCount: (game: GameSummary) =>
+			{
+				return Math.max(game.linescore.inning.length - game.linescore.startingInning, 9);
+			},
+			getLinescoreItem: (game: GameSummary, inningIndex: number, homeOrAway: string) =>
+			{
+				let realInningIndex = inningIndex + game.linescore.startingInning;
+
+				let emptyInning = {
+					home: "",
+					away: ""
+				};
+
+				let inning: IInning = (emptyInning as any) as IInning;
+				
+
+				if (game.linescore && game.linescore.inning && game.linescore.inning.length > realInningIndex)
+				{
+					inning = game.linescore.inning[realInningIndex];
+				}
+
+				let homeInningText = game.status.status === "Final"
+					? inning.home || "▨"
+					: inning.home;
+
+				return homeOrAway === "home"
+					? linescoreItem(homeInningText)
+					: linescoreItem(inning.away);
+			},
+			getLinescoreLabel: (game: GameSummary, inningIndex: number) =>
+			{
+				let realInningIndex = inningIndex + game.linescore.startingInning;
+
+				return realInningIndex;
 			}
 		}
 	});
