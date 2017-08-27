6d5c04f548d4838295b0af5c5c76ece41be9148f
Jake Lauer <jake.lauer@gmail.com>
2017-08-26 22:19:56 -0700

    Updates to comply with MLB terms

--- BaseballTheater/Areas/App/Scripts/Components/highlight.ts
+++ BaseballTheater/Areas/App/Scripts/Components/highlight.ts
@@ -6,115 +6,124 @@
 		label: string;
 	}
 
-	var getLinks = (game: IHighlight): ILink[] =>
-	{
-		const qkRegex = /(\d{4}K)/;
-		var links = game.url;
-		var q1200k: ILink = null;
-
-		// For some reason, Safari doesn't like this particular 
-		// variable when it's a 'let' when it's minified. 
-		// ReSharper disable once VariableCanBeMadeLet
-		for (var link of links)
-		{
-			const matches = link.__text.match(qkRegex);
-			if (matches && matches.length > 0)
-			{
-				const label = matches[0] as string;
-				q1200k = {
-					url: link.__text.replace("http:", location.protocol),
-					label: label
-				};
-			}
-		}
-
-		var validLinks = [];
-
-		if (q1200k !== null)
-		{
-			const q1800K = {
-				url: q1200k.url.replace(qkRegex, "1800K").replace("http:", location.protocol),
-				label: "1800K"
-			};
-
-			const q2500K = {
-				url: q1200k.url.replace(qkRegex, "2500K").replace("http:", location.protocol),
-				label: "2500K"
-			};
-
-			validLinks.push(q1200k, q1800K, q2500K);
-		}
-
-		return validLinks;
-	};
-
 	Vue.component("highlight",
+		{
+			template: $("template#highlight").html(),
+			props: ["highlight"],
+			methods: {
+				getLinks: (highlight: IHighlight) => HighlightModule.getLinks(highlight),
+				getDefaultUrl: (highlight: IHighlight) => HighlightModule.getDefaultUrl(highlight),
+				getTitle: (highlight: IHighlight) => HighlightModule.getTitle(highlight),
+				getDefaultThumb: (highlight: IHighlight) => HighlightModule.getDefaultThumb(highlight),
+				getMlbLink: (highlight: IHighlight) => HighlightModule.getMlbLink(highlight),
+				showMlbFrame: (highlight: IHighlight) => HighlightModule.showMlbFrame(highlight)
+			}
+		});
+
+	module HighlightModule
 	{
-		template: $("template#highlight").html(),
-		props: ["highlight"],
-		methods: {
-			getLinks,
-			getDefaultUrl: (highlight: IHighlight) =>
-			{
-				var links = getLinks(highlight);
+		export function getLinks(game: IHighlight)
+		{
 
-				return links[0].url;
-			},
-			getTitle: (highlight: IHighlight) =>
+			const qkRegex = /(\d{4}K)/;
+			var links = game.url;
+			var q1200k: ILink = null;
+
+			// For some reason, Safari doesn't like this particular 
+			// variable when it's a 'let' when it's minified. 
+			// ReSharper disable once VariableCanBeMadeLet
+			for (var link of links)
 			{
-				return highlight.recap && App.Instance.settingsVueData.hideScores
-					? "Recap (score hidden)"
-					: highlight.headline;
-			},
-			getDefaultThumb: (highlight: IHighlight) =>
-			{
-				let thumbFinal: string = highlight.thumb;
-				if (highlight.thumbnails != null && highlight.thumbnails.thumb != null && highlight.thumbnails.thumb.length > 0)
+				const matches = link.__text.match(qkRegex);
+				if (matches && matches.length > 0)
 				{
-					const thumbs = highlight.thumbnails.thumb;
+					const label = matches[0] as string;
+					q1200k = {
+						url: link.__text.replace("http:", location.protocol),
+						label: label
+					};
+				}
+			}
 
-					if (App.clientNetSpeed !== NetSpeed.Fast)
+			var validLinks = [];
+
+			if (q1200k !== null)
+			{
+				const q1800K = {
+					url: q1200k.url.replace(qkRegex, "1800K").replace("http:", location.protocol),
+					label: "1800K"
+				};
+
+				const q2500K = {
+					url: q1200k.url.replace(qkRegex, "2500K").replace("http:", location.protocol),
+					label: "2500K"
+				};
+
+				validLinks.push(q1200k, q1800K, q2500K);
+			}
+
+			return validLinks;
+		}
+
+		export function getDefaultUrl(highlight: IHighlight)
+		{
+			var links = getLinks(highlight);
+
+			return links[0].url;
+		}
+
+		export function getTitle(highlight: IHighlight)
+		{
+			return highlight.recap && App.Instance.settingsVueData.hideScores
+				? "Recap (score hidden)"
+				: highlight.headline;
+		}
+
+		export function showMlbFrame(highlight: IHighlight)
+		{
+			var modal = new Modal()
+		}
+
+		export function getMlbLink(highlight: IHighlight)
+		{
+			/*var $playCover = $(event.currentTarget);
+			var $video = $playCover.siblings("video");
+			($video[0] as HTMLVideoElement).play();*/
+
+			const cId = highlight.id;
+			const mlbLink = `https://www.mlb.com/video/c-${cId}`;
+			return mlbLink;
+		}
+
+		export function getDefaultThumb(highlight: IHighlight)
+		{
+			let thumbFinal: string = highlight.thumb;
+			if (highlight.thumbnails != null && highlight.thumbnails.thumb != null && highlight.thumbnails.thumb.length > 0)
+			{
+				const thumbs = highlight.thumbnails.thumb;
+
+				if (App.clientNetSpeed !== NetSpeed.Fast)
+				{
+					let jpgSearch = "47.jpg";
+
+					if (App.clientNetSpeed === NetSpeed.Slow)
 					{
-						let jpgSearch = "47.jpg";
-
-						if (App.clientNetSpeed === NetSpeed.Slow)
-						{
-							jpgSearch = "52.jpg";
-						}
-
-						for (let thumb of thumbs)
-						{
-							if (Utility.endsWith(thumb.__text, jpgSearch))
-							{
-								thumbFinal = thumb.__text;
-							}
-						}
+						jpgSearch = "52.jpg";
 					}
 
-					thumbFinal = thumbs[thumbs.length - 2].__text;
+					for (let thumb of thumbs)
+					{
+						if (Utility.endsWith(thumb.__text, jpgSearch))
+						{
+							thumbFinal = thumb.__text;
+						}
+					}
 				}
 
-				return thumbFinal.replace("http:", location.protocol);
-			},
-			playVideo: (event: Event) =>
-			{
-				var $playCover = $(event.currentTarget);
-				var $video = $playCover.siblings("video");
-				($video[0] as HTMLVideoElement).play();
-			},
-			pauseVideo: (event: Event) =>
-			{
-				var video = event.currentTarget as HTMLVideoElement;
-				video.pause();
-			},
-			onPlay: (highlight: IHighlight) =>
-			{
-				highlight.isPlaying = true;
-			},
-			onPause: (highlight: IHighlight) =>
-			{
-				highlight.isPlaying = false;
+				thumbFinal = thumbs[thumbs.length - 2].__text;
 			}
+
+			return thumbFinal.replace("http:", location.protocol);
 		}
-	});
+	}
 }
