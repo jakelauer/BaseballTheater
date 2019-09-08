8744414e7938d2d28ed32ae8a22e35d192d892eb
Jake Lauer <jake.lauer@gmail.com>
2019-09-08 00:14:24 -0700

    Adding support for API keys (to protect Playback endpoints and ensure exclusivity)

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,8 @@
 import {PlaybackEndpointMap} from "./playback-endpoints";
 import express from "express";
 import * as path from "path";
+import {NextFunction, Request, Response} from "express-serve-static-core";
+import {Utils} from "./utils";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,12 +14,29 @@ const server = app.listen(port, () => console.log(`Listening on port ${port}`));
 server.setTimeout(10000);
 
 // create a GET route
-app.get("/express_backend", (req, res) => {
-  res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
+app.get("/express_backend", (req, res) =>
+{
+	res.send({express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT"});
 });
 
-const playback = Object.keys(PlaybackEndpointMap).map(endpointPath => {
-  const callback = PlaybackEndpointMap[endpointPath];
+Object.keys(PlaybackEndpointMap).map(endpointPath =>
+{
+	const callback = PlaybackEndpointMap[endpointPath];
 
-  app.get(endpointPath, callback);
+	app.get(endpointPath, (req: Request, res: Response, next: NextFunction) =>
+	{
+		try
+		{
+			const valid = Utils.validateApiKey(req);
+			if (!valid)
+			{
+				throw new Error("API Key not valid");
+			}
+			callback(req, res, next);
+		}
+		catch (e)
+		{
+			res.status(500).json({error: e.toString()});
+		}
+	});
 });
