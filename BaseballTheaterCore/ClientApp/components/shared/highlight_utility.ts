cbaeb55568a549030fc9c3c9efb7a3706403cb29
Jake Lauer <jake.lauer@gmail.com>
2020-02-17 12:08:05 -0800

    update

--- BaseballTheaterCore/ClientApp/components/shared/highlight_utility.ts
+++ BaseballTheaterCore/ClientApp/components/shared/highlight_utility.ts
@@ -1,15 +1,13 @@
-﻿import {Utility} from "@Utility/index";
+﻿import { Utility } from "@Utility/index";
 import * as moment from "moment"
 
-enum HighlightThumbQuality
-{
+enum HighlightThumbQuality {
 	Low,
 	Med,
 	High,
 }
 
-export interface IHighlightDisplay
-{
+export interface IHighlightDisplay {
 	thumb: string | null;
 	links: ILink[];
 	videoUrl: string;
@@ -20,26 +18,21 @@ export interface IHighlightDisplay
 	condensed: boolean;
 }
 
-export interface ILink
-{
+export interface ILink {
 	url: string;
 	label: string;
 }
 
-export class HighlightUtility
-{
-	public static isRecap(highlight: MediaItem)
-	{
+export class HighlightUtility {
+	public static isRecap(highlight: MediaItem) {
 		return highlight.keywordsAll.some(a => a.value.toLocaleLowerCase().includes("recap"));
 	}
 
-	public static isCondensed(highlight: MediaItem)
-	{
+	public static isCondensed(highlight: MediaItem) {
 		return highlight.keywordsAll.some(a => a.value.toLocaleLowerCase().includes("condensed"));
 	}
 
-	public static getDisplayProps(highlight: MediaItem, hideScores: boolean): IHighlightDisplay | null
-	{
+	public static getDisplayProps(highlight: MediaItem, hideScores: boolean): IHighlightDisplay | null {
 		let displayProps: IHighlightDisplay | null = {
 			thumb: "",
 			links: [],
@@ -51,47 +44,48 @@ export class HighlightUtility
 			condensed: false
 		};
 
-		if (highlight && highlight.playbacks)
-        {
-            if (highlight.image && highlight.image.cuts) {
-                const images = highlight.image.cuts;
-                const img = images.find(a => a.aspectRatio === "16:9" && a.width < 1000 && a.width > 500)
-                    || images.find(a => a.aspectRatio === "16:9")
-                    || images[0];
+		if (highlight && highlight.playbacks) {
+			try {
+				if (highlight.image && highlight.image.cuts) {
+					const images = highlight.image.cuts;
+					const img = images.find(a => a.aspectRatio === "16:9" && a.width < 1000 && a.width > 500)
+						|| images.find(a => a.aspectRatio === "16:9")
+						|| images[0];
 
-                displayProps.thumb = img.src;
-            }
+					displayProps.thumb = img.src;
+				}
 
-			displayProps.links = highlight.playbacks.filter(a => a.name.includes("FLASH")).map(item => ({
-				url: item.url,
-				label: item.name.match(/[0-9]+K/gi)[0]
-            }));
+				displayProps.links = highlight.playbacks.filter(a => a.name.includes("FLASH")).map(item => ({
+					url: item.url,
+					label: item.name.match(/[0-9]+K/gi)[0]
+				}));
 
-            const matchingVideo = highlight.playbacks.find(a => a.url.includes("1800K"))
-                || highlight.playbacks.find(a => a.url.includes("mp4"));
+				const matchingVideo = highlight.playbacks.find(a => a.url.includes("1800K"))
+					|| highlight.playbacks.find(a => a.url.includes("mp4"));
 
-            displayProps.videoUrl = matchingVideo.url;
+				displayProps.videoUrl = matchingVideo.url;
 
-			const isRecap = HighlightUtility.isRecap(highlight);
-			const isCondensed = HighlightUtility.isCondensed(highlight);
+				const isRecap = HighlightUtility.isRecap(highlight);
+				const isCondensed = HighlightUtility.isCondensed(highlight);
 
-			displayProps.recap = isRecap;
-			displayProps.condensed = isCondensed;
+				displayProps.recap = isRecap;
+				displayProps.condensed = isCondensed;
 
-			if (isRecap)
-			{
-				displayProps.overrideTitle = "Recap";
+				if (isRecap) {
+					displayProps.overrideTitle = "Recap";
+				}
+
+				if (isCondensed) {
+					displayProps.overrideTitle = "Condensed Game";
+				}
+
+				displayProps.headline = (isRecap && hideScores) ? "Recap" : highlight.headline;
 			}
-
-			if (isCondensed)
-			{
-				displayProps.overrideTitle = "Condensed Game";
+			catch (e) {
+				displayProps = null;
 			}
-
-			displayProps.headline = (isRecap && hideScores) ? "Recap" : highlight.headline;
 		}
-		else
-		{
+		else {
 			displayProps = null;
 		}
 
