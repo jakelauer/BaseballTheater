ef27bf544b395407e2ef76ceaecb9d986a19f923
Jake Lauer <jake.lauer@gmail.com>
2020-02-17 12:08:05 -0800

    update

--- BaseballTheaterCore/ClientApp/components/shared/highlight.tsx
+++ BaseballTheaterCore/ClientApp/components/shared/highlight.tsx
@@ -1,12 +1,12 @@
-﻿import {Link} from "react-router-dom";
-import {Card} from "antd";
+﻿import { Link } from "react-router-dom";
+import { Card } from "antd";
 import React = require("react");
-import {HighlightUtility, IHighlightDisplay} from "./highlight_utility";
+import { HighlightUtility, IHighlightDisplay } from "./highlight_utility";
 import * as moment from "moment"
 import { ISearchResult } from "../Search/search";
+import { HiddenErrorBoundary } from "../Base/HiddenErrorBoundary";
 
-interface IHighlightProps
-{
+interface IHighlightProps {
 	renderDate: boolean;
 	highlight: MediaItem | ISearchResult;
 	hideScores: boolean;
@@ -14,43 +14,37 @@ interface IHighlightProps
 
 export class Highlight extends React.Component<IHighlightProps, any>
 {
-	private get highlight()
-	{
+	private get highlight() {
 		return (this.props.highlight as ISearchResult).highlight || (this.props.highlight as MediaItem);
 	}
 
-	private get searchResult(): any | null
-	{
+	private get searchResult(): any | null {
 		const asSearchResult = this.props.highlight as ISearchResult;
 		const isSearchResult = !!asSearchResult.highlight;
 		return isSearchResult ? asSearchResult : null;
 	}
 
-	private getGameLink()
-	{
+	private getGameLink() {
 		const dateString = moment(this.highlight.date).format("YYYYMMDD");
 		const gameId = this.searchResult ? String(this.searchResult.gameId) : null
 		return gameId ? `/game/${dateString}/${gameId}` : "javascript:void(0)";
 	}
 
-	public render()
-	{
+	public render() {
 		const displayProps = HighlightUtility.getDisplayProps(this.highlight, this.props.hideScores); //, this.searchResult);
 
-		if (!displayProps)
-		{
-			return <div/>;
+		if (!displayProps) {
+			return <div />;
 		}
 
-		const thumbStyle = {backgroundImage: `url(${displayProps.thumb})`};
+		const thumbStyle = { backgroundImage: `url(${displayProps.thumb})` };
 
-		const links = displayProps.links.map((link, i) =>
-		{
+		const links = displayProps.links.map((link, i) => {
 			return <a href={link.url} target="_blank" key={i} rel={"noreferrer"}>{link.label}</a>;
 		});
 
 		const cover = <a href={displayProps.videoUrl} target={`_blank`} rel={"noreferrer"}>
-			<div className={`thumb`} style={thumbStyle}/>
+			<div className={`thumb`} style={thumbStyle} />
 		</a>;
 
 		const dateString = moment(this.highlight.date).format("MMM D, YYYY");
@@ -59,24 +53,26 @@ export class Highlight extends React.Component<IHighlightProps, any>
 				{dateString}
 			</Link>
 			: null;
-			
+
 		const title = <React.Fragment>
 			{displayProps.headline}
-			<br/>
+			<br />
 			{dateRendered}
 		</React.Fragment>;
 
 		return (
-			<div className={`highlight`}>
-				<Card
-					cover={cover}
-					actions={links}
-				>
-					<Card.Meta
-						title={title}
-					/>
-				</Card>
-			</div>
+			<HiddenErrorBoundary>
+				<div className={`highlight`}>
+					<Card
+						cover={cover}
+						actions={links}
+					>
+						<Card.Meta
+							title={title}
+						/>
+					</Card>
+				</div>
+			</HiddenErrorBoundary>
 		);
 	}
 }
--- BaseballTheaterCore/Node/Populator.ts
+++ BaseballTheaterCore/Node/Populator.ts
@@ -5,92 +5,92 @@ import Internal_LiveGameCreator from "@MlbDataServer/Internal_LiveGameCreator";
 import { HighlightUtility } from "ClientApp/components/shared/highlight_utility";
 
 export interface PopulatorArgs {
-    dateString?: string;
-    loopDates?: boolean;
+	dateString?: string;
+	loopDates?: boolean;
 }
 
 class PopulatorInternal {
-    private date: moment.Moment;
-    private results: { [key: string]: MediaItem[] } = {};
-    private allPromises: Promise<any>[] = [];
-    public static Instance = new PopulatorInternal();
+	private date: moment.Moment;
+	private results: { [key: string]: MediaItem[] } = {};
+	private allPromises: Promise<any>[] = [];
+	public static Instance = new PopulatorInternal();
 
-    public initialize(args: PopulatorArgs) {
-        this.date = args.dateString
-            ? moment(args.dateString, "YYYYMMDD")
-            : moment().add(-5, "days");
+	public initialize(args: PopulatorArgs) {
+		this.date = args.dateString
+			? moment(args.dateString, "YYYYMMDD")
+			: moment().add(-5, "days");
 
-        this.loadHighlights();
-    }
+		this.loadHighlights();
+	}
 
-    private async loadHighlights() {
-        const today = moment();
-        let lastDay = this.date;
+	private async loadHighlights() {
+		const today = moment();
+		let lastDay = this.date;
 
-        console.log("===start loop===")
-        while (lastDay.isBefore(today)) {
-            const highlights = await this.loadHighlightsForDate(lastDay);
-            const dateString = lastDay.format("YYYYMMDD");
-            const jsonResult = JSON.stringify(highlights);
-            fs.writeFile("C:\\highlightdata\\" + dateString + ".json", jsonResult, "utf8");
-            lastDay = lastDay.add(1, "day");
-        }
-        console.log("===end loop===");
-    }
+		console.log("===start loop===")
+		while (lastDay.isBefore(today)) {
+			const highlights = await this.loadHighlightsForDate(lastDay);
+			const dateString = lastDay.format("YYYYMMDD");
+			const jsonResult = JSON.stringify(highlights);
+			fs.writeFile("C:\\highlightdata\\" + dateString + ".json", jsonResult, "utf8");
+			lastDay = lastDay.add(1, "day");
+		}
+		console.log("===end loop===");
+	}
 
-    private async loadHighlightsForDate(date: moment.Moment) {
-        const dateString = date.format("YYYYMMDD");
-        const summaries = Internal_GameSummaryCreator.getSummaryCollectionNode(date);
+	private async loadHighlightsForDate(date: moment.Moment) {
+		const dateString = date.format("YYYYMMDD");
+		const summaries = Internal_GameSummaryCreator.getSummaryCollectionNode(date);
 
-        const data = await summaries;
+		const data = await summaries;
 
-        if (data.games.game) {
-            console.log("games found for " + dateString + ": " + data.games.game.length);
-        }
+		if (data.games.game) {
+			console.log("games found for " + dateString + ": " + data.games.game.length);
+		}
 
-        const allHighlights = [];
-        for (const game of data.games.game) {
-            const highlights = await this.loadHighlightsForGame(game.game_pk);
-            allHighlights.push(...highlights);
-        }
+		const allHighlights = [];
+		for (const game of data.games.game) {
+			const highlights = await this.loadHighlightsForGame(game.game_pk);
+			allHighlights.push(...highlights);
+		}
 
 
-        return allHighlights;
-    }
+		return allHighlights;
+	}
 
-    private async loadHighlightsForGame(game_pk: string) {
-        console.log(`game: ${game_pk}`);
+	private async loadHighlightsForGame(game_pk: string) {
+		console.log(`game: ${game_pk}`);
 
-        const data = await Internal_LiveGameCreator.getGameMediaNode(game_pk);
+		const data = await Internal_LiveGameCreator.getGameMediaNode(game_pk);
 
-        let highlights: IHighlightSearchItem[] = [];
-        const hasHighlights = !!(data.highlights && data.highlights.highlights && data.highlights.highlights.items);
-        console.log(`hasHighlights: ${hasHighlights}`)
-        if (data.highlights && data.highlights.highlights && data.highlights.highlights.items) {
-            console.log(data.highlights.highlights.items.length);
-            highlights = data.highlights.highlights.items.map(highlight => {
+		let highlights: IHighlightSearchItem[] = [];
+		const hasHighlights = !!(data.highlights && data.highlights.highlights && data.highlights.highlights.items);
+		console.log(`hasHighlights: ${hasHighlights}`)
+		if (data.highlights && data.highlights.highlights && data.highlights.highlights.items) {
+			console.log(data.highlights.highlights.items.length);
+			highlights = data.highlights.highlights.items.map(highlight => {
 
-                const displayProps = HighlightUtility.getDisplayProps(highlight, false);
-                if (displayProps) {
-                    console.log("has displayprops");
-                    const matchingCut = highlight.image.cuts.find(a => a.src === displayProps.thumb);
-                    highlight.image.cuts = [matchingCut];
-                }
+				const displayProps = HighlightUtility.getDisplayProps(highlight, false);
+				if (displayProps) {
+					console.log("has displayprops");
+					const matchingCut = highlight.image.cuts.find(a => a.src === displayProps.thumb);
+					highlight.image.cuts = [matchingCut];
+				}
 
-                return {
-                    highlight,
-                    game_pk
-                };
-            })
-        }
+				return {
+					highlight,
+					game_pk
+				};
+			})
+		}
 
-        return highlights;
-    }
+		return highlights;
+	}
 }
 
 interface IHighlightSearchItem {
-    highlight: MediaItem,
-    game_pk: string
+	highlight: MediaItem,
+	game_pk: string
 }
 
 export const Populator = PopulatorInternal.Instance;
