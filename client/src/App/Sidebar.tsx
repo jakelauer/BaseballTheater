364104ddaf6d5a3177e9aa3ba2ebb0bb64a1655e
Jake Lauer <jakelauer@Jake-MacBook-M2-2.local>
2024-02-29 10:20:15 -0800

    nvmrc

--- client/src/App/Sidebar.tsx
+++ client/src/App/Sidebar.tsx
@@ -1,40 +1,43 @@
-import { Button, ListItemAvatar } from '@material-ui/core';
-import Collapse from '@material-ui/core/Collapse';
-import Container from '@material-ui/core/Container';
-import Divider from '@material-ui/core/Divider';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import withStyles from '@material-ui/core/styles/withStyles';
-import Typography from '@material-ui/core/Typography';
-import { Equalizer, ReportProblem, Search } from '@material-ui/icons';
-import EventIcon from '@material-ui/icons/Event';
-import SettingsIcon from '@material-ui/icons/Settings';
-import SportsBaseball from '@material-ui/icons/SportsBaseball';
-import carlsplace from 'assets/backers/carlsplace.png';
-import highinsky from 'assets/backers/highinsky.png';
-import storeporter from 'assets/backers/storeporter.png';
-import { Teams } from 'baseball-theater-engine';
-import cookies from 'browser-cookies';
-import classNames from 'classnames';
-import moment from 'moment';
-import React from 'react';
-import { FaVideo } from 'react-icons/all';
-import { Link } from 'react-router-dom';
+import { Teams } from "baseball-theater-engine";
+import cookies from "browser-cookies";
+import classNames from "classnames";
+import moment from "moment";
+import React from "react";
+import { FaVideo } from "react-icons/all";
+import { Link } from "react-router-dom";
 
-import { AuthDataStore, BackerType, IAuthContext } from '../Global/AuthDataStore';
-import { SiteRoutes } from '../Global/Routes/Routes';
-import { ISettingsDataStorePayload, SettingsDataStore } from '../Global/Settings/SettingsDataStore';
-import styles from './App.module.scss';
+import { Button, ListItemAvatar } from "@material-ui/core";
+import Collapse from "@material-ui/core/Collapse";
+import Container from "@material-ui/core/Container";
+import Divider from "@material-ui/core/Divider";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import withStyles from "@material-ui/core/styles/withStyles";
+import Typography from "@material-ui/core/Typography";
+import { Equalizer, ReportProblem, Search } from "@material-ui/icons";
+import EventIcon from "@material-ui/icons/Event";
+import SettingsIcon from "@material-ui/icons/Settings";
+import SportsBaseball from "@material-ui/icons/SportsBaseball";
 
-interface ISidebarProps
-{
+import {
+	AuthDataStore,
+	BackerType,
+	IAuthContext,
+} from "../Global/AuthDataStore";
+import { SiteRoutes } from "../Global/Routes/Routes";
+import {
+	ISettingsDataStorePayload,
+	SettingsDataStore,
+} from "../Global/Settings/SettingsDataStore";
+import styles from "./App.module.scss";
+
+interface ISidebarProps {
 	authContext: IAuthContext;
 }
 
-interface DefaultProps
-{
+interface DefaultProps {
 	onNavigate?: () => void;
 }
 
@@ -42,8 +45,8 @@ const PatreonButton = withStyles({
 	root: {
 		backgroundColor: "#f96854",
 		"&:hover": {
-			backgroundColor: "#ff8777"
-		}
+			backgroundColor: "#ff8777",
+		},
 	},
 })(Button);
 
@@ -51,25 +54,22 @@ const LogInButton = withStyles({
 	root: {
 		backgroundColor: "#DDDDDD",
 		"&:hover": {
-			backgroundColor: "#EEEEEE"
-		}
-	}
+			backgroundColor: "#EEEEEE",
+		},
+	},
 })(Button);
 
 type Props = ISidebarProps & DefaultProps;
 type State = ISidebarState;
 
-interface ISidebarState
-{
+interface ISidebarState {
 	videoTagsOpen: boolean;
 	settings: ISettingsDataStorePayload;
 	authContext: IAuthContext;
 }
 
-class Sidebar extends React.Component<Props, State>
-{
-	constructor(props: Props)
-	{
+class Sidebar extends React.Component<Props, State> {
+	constructor(props: Props) {
 		super(props);
 
 		this.state = {
@@ -80,51 +80,55 @@ class Sidebar extends React.Component<Props, State>
 	}
 
 	public static defaultProps: DefaultProps = {
-		onNavigate: () =>
-		{
-		}
+		onNavigate: () => {},
 	};
 
-	private onFeaturedVideosClick = () =>
-	{
+	private onFeaturedVideosClick = () => {
 		this.setState({
-			videoTagsOpen: !this.state.videoTagsOpen
-		})
+			videoTagsOpen: !this.state.videoTagsOpen,
+		});
 	};
 
-	private get patreonUrl()
-	{
-		const clientId = "4f3fb1d9df8f53406f60617258e66ef5cba993b1aa72d2e32e66a1b5be0b9008";
-		const host = window.location.hostname === "jlauer.local" ? "jlauer.local:8000" : window.location.hostname;
-		const protocol = window.location.hostname === "jlauer.local" ? "http:" : "https:";
+	private get patreonUrl() {
+		const clientId =
+			"4f3fb1d9df8f53406f60617258e66ef5cba993b1aa72d2e32e66a1b5be0b9008";
+		const host =
+			window.location.hostname === "jlauer.local"
+				? "jlauer.local:8000"
+				: window.location.hostname;
+		const protocol =
+			window.location.hostname === "jlauer.local" ? "http:" : "https:";
 		const redirectUri = `${protocol}//${host}/auth/redirect`;
 		const scopes = ["users", "pledges-to-me", "my-campaign"];
 		const state = encodeURIComponent(window.location.pathname);
-		return `https://www.patreon.com/oauth2/authorize?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scopes.join(" ")}&state=${state}`;
+		return `https://www.patreon.com/oauth2/authorize?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(
+			redirectUri
+		)}&scope=${scopes.join(" ")}&state=${state}`;
 	}
 
-	private logOut = () =>
-	{
+	private logOut = () => {
 		cookies.erase("auth");
 
 		AuthDataStore.refresh();
 	};
 
-	public componentDidMount(): void
-	{
-		AuthDataStore.listen(data => this.setState({
-			authContext: data
-		}));
+	public componentDidMount(): void {
+		AuthDataStore.listen((data) =>
+			this.setState({
+				authContext: data,
+			})
+		);
 
-		SettingsDataStore.listen(data => this.setState({
-			settings: data
-		}));
+		SettingsDataStore.listen((data) =>
+			this.setState({
+				settings: data,
+			})
+		);
 	}
 
-	public render()
-	{
-		const {videoTagsOpen} = this.state;
-		const {authContext, onNavigate} = this.props;
+	public render() {
+		const { videoTagsOpen } = this.state;
+		const { authContext, onNavigate } = this.props;
 
 		const isStarBacker = AuthDataStore.hasLevel(BackerType.StarBacker);
 
@@ -134,17 +138,31 @@ class Sidebar extends React.Component<Props, State>
 					Baseball Theater
 				</Link>
 				<List component={"nav"}>
-					{isStarBacker && this.state.settings.favoriteTeams.length > 0 && (
-						<React.Fragment>
-							{this.state.settings.favoriteTeams.map(team => (
-								<MenuItem key={team} onClick={onNavigate} icon={<SportsBaseball/>} path={SiteRoutes.Team.resolve({team})}>
-									{Teams.TeamList[team]}
-								</MenuItem>
-							))}
-							<Divider/>
-						</React.Fragment>
-					)}
-					<MenuItem onClick={onNavigate} icon={<EventIcon/>} path={SiteRoutes.GamesRoot.resolve({})}>
+					{isStarBacker &&
+						this.state.settings.favoriteTeams.length > 0 && (
+							<React.Fragment>
+								{this.state.settings.favoriteTeams.map(
+									(team) => (
+										<MenuItem
+											key={team}
+											onClick={onNavigate}
+											icon={<SportsBaseball />}
+											path={SiteRoutes.Team.resolve({
+												team,
+											})}
+										>
+											{Teams.TeamList[team]}
+										</MenuItem>
+									)
+								)}
+								<Divider />
+							</React.Fragment>
+						)}
+					<MenuItem
+						onClick={onNavigate}
+						icon={<EventIcon />}
+						path={SiteRoutes.GamesRoot.resolve({})}
+					>
 						Games
 					</MenuItem>
 					{/*<MenuItem icon={<PlayCircleFilled/>} end={videoTagsOpen ? <ExpandLess/> : <ExpandMore/>} onClick={this.onFeaturedVideosClick}>
@@ -152,60 +170,104 @@ class Sidebar extends React.Component<Props, State>
 					 </MenuItem>*/}
 					<Collapse in={videoTagsOpen} timeout="auto">
 						<List component="div" disablePadding>
-							<ListItem onClick={onNavigate} button component={p => <Link {...p} to={SiteRoutes.FeaturedVideos.resolve({category: "Recap"})}/>}>
-								<ListItemIcon className={styles.indent}><FaVideo/></ListItemIcon>
-								<ListItemText primary={"Recaps"}/>
+							<ListItem
+								onClick={onNavigate}
+								button
+								component={(p) => (
+									<Link
+										{...p}
+										to={SiteRoutes.FeaturedVideos.resolve({
+											category: "Recap",
+										})}
+									/>
+								)}
+							>
+								<ListItemIcon className={styles.indent}>
+									<FaVideo />
+								</ListItemIcon>
+								<ListItemText primary={"Recaps"} />
 							</ListItem>
 						</List>
 					</Collapse>
-					<MenuItem onClick={onNavigate} icon={<Equalizer/>} path={SiteRoutes.Standings.resolve({year: moment().year().toString()})}>
+					<MenuItem
+						onClick={onNavigate}
+						icon={<Equalizer />}
+						path={SiteRoutes.Standings.resolve({
+							year: moment().year().toString(),
+						})}
+					>
 						Standings
 					</MenuItem>
-					<MenuItem onClick={onNavigate} icon={<Search/>} path={SiteRoutes.Search.resolve({})}>
+					<MenuItem
+						onClick={onNavigate}
+						icon={<Search />}
+						path={SiteRoutes.Search.resolve({})}
+					>
 						Search
 					</MenuItem>
-					<MenuItem onClick={onNavigate} icon={<SettingsIcon/>} path={SiteRoutes.Settings.resolve({})}>
+					<MenuItem
+						onClick={onNavigate}
+						icon={<SettingsIcon />}
+						path={SiteRoutes.Settings.resolve({})}
+					>
 						Settings
 					</MenuItem>
 				</List>
 				{!authContext.authorized && authContext.loaded && (
 					<React.Fragment>
-						<a className={styles.patreonButtonLink} href={"https://www.patreon.com/jakelauer"}>
-							<PatreonButton className={styles.patreonJoin} style={{width: "100%"}}>
+						<a
+							className={styles.patreonButtonLink}
+							href={"https://www.patreon.com/jakelauer"}
+						>
+							<PatreonButton
+								className={styles.patreonJoin}
+								style={{ width: "100%" }}
+							>
 								Join as a Patron
 							</PatreonButton>
 						</a>
-						<a className={styles.patreonButtonLink} href={this.patreonUrl}>
-							<LogInButton className={styles.patreonButton} style={{width: "100%"}}>
+						<a
+							className={styles.patreonButtonLink}
+							href={this.patreonUrl}
+						>
+							<LogInButton
+								className={styles.patreonButton}
+								style={{ width: "100%" }}
+							>
 								Patron Log In
 							</LogInButton>
 						</a>
 					</React.Fragment>
-				)
-				}
+				)}
 				{authContext.authorized && authContext.loaded && (
-					<Button className={styles.patreonButton} onClick={this.logOut}>
+					<Button
+						className={styles.patreonButton}
+						onClick={this.logOut}
+					>
 						Log out
 					</Button>
 				)}
-				<div className={styles.sponsors}>
-					<p>Diamond Sponsors</p>
-					<Sponsor imagePath={carlsplace} url={"https://www.carlofet.com/?utm_medium=advertising&utm_source=baseballtheater&utm_campaign=baseballtheater"}/>
-					<Sponsor imagePath={highinsky} url={"https://highinsky.com"}/>
-					<Sponsor imagePath={storeporter} url={"https://storeporter.com"}/>
-					<Sponsor url={"https://www.patreon.com/jakelauer"}/>
-				</div>
 				<div className={styles.bottom}>
 					<div className={styles.report}>
-						<a target={"_blank"} rel={"noreferrer nofollow"} href={"https://github.com/jakelauer/BaseballTheater/issues/new"}>
-							<ReportProblem/> <span>Report a Problem</span>
+						<a
+							target={"_blank"}
+							rel={"noreferrer nofollow"}
+							href={
+								"https://github.com/jakelauer/BaseballTheater/issues/new"
+							}
+						>
+							<ReportProblem /> <span>Report a Problem</span>
 						</a>
 					</div>
 				</div>
 
-				<Container style={{textAlign: "center", padding: "2rem"}}>
-					&copy; {moment().year()}. Created by <a href={"http://jakelauer.com"}>Jake Lauer</a> (<a href={"https://www.reddit.com/user/hellocontrol_"}>HelloControl_</a>).
-					Go Cubs.
+				<Container style={{ textAlign: "center", padding: "2rem" }}>
+					&copy; {moment().year()}. Created by{" "}
+					<a href={"http://jakelauer.com"}>Jake Lauer</a> (
+					<a href={"https://www.reddit.com/user/hellocontrol_"}>
+						HelloControl_
+					</a>
+					). Go Cubs.
 				</Container>
 			</>
 		);
@@ -215,21 +277,23 @@ class Sidebar extends React.Component<Props, State>
 const MenuItem = (props: {
 	icon: React.ReactElement;
 	path?: string;
-	children?: React.ReactNode,
-	end?: React.ReactNode,
-	textStyle?: React.CSSProperties,
-	onClick?: () => void
-}) =>
-{
+	children?: React.ReactNode;
+	end?: React.ReactNode;
+	textStyle?: React.CSSProperties;
+	onClick?: () => void;
+}) => {
 	const component = props.path
-		? (p: any) => <Link {...p} to={props.path}/>
+		? (p: any) => <Link {...p} to={props.path} />
 		: undefined;
 
 	return (
-		<ListItem button color={"primary"} component={component} onClick={props.onClick}>
-			<ListItemAvatar>
-				{props.icon}
-			</ListItemAvatar>
+		<ListItem
+			button
+			color={"primary"}
+			component={component}
+			onClick={props.onClick}
+		>
+			<ListItemAvatar>{props.icon}</ListItemAvatar>
 			<ListItemText style={props.textStyle}>
 				{props.children}
 			</ListItemText>
@@ -238,33 +302,35 @@ const MenuItem = (props: {
 	);
 };
 
-interface ISponsorProps
-{
+interface ISponsorProps {
 	imagePath?: string;
 	url?: string;
 }
 
-const Sponsor: React.FC<ISponsorProps> = (props) =>
-{
+const Sponsor: React.FC<ISponsorProps> = (props) => {
 	const className = classNames(styles.sponsor, {
 		[styles.hasLink]: !!props.url,
-		[styles.hasImage]: !!props.imagePath
+		[styles.hasImage]: !!props.imagePath,
 	});
 
 	let style: React.CSSProperties;
-	if (props.imagePath)
-	{
+	if (props.imagePath) {
 		style = {
-			backgroundImage: `url(${props.imagePath})`
+			backgroundImage: `url(${props.imagePath})`,
 		};
 	}
 
 	return (
-		<a className={className} style={style} href={props.url} target={"_blank"}>
+		<a
+			className={className}
+			style={style}
+			href={props.url}
+			target={"_blank"}
+			rel="noreferrer"
+		>
 			{!props.imagePath && <Typography>+ Diamond Sponsor</Typography>}
 		</a>
 	);
 };
-
 
 export default Sidebar;
